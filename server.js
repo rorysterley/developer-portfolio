55bdd14f775738c997468c4c9d7522e0ffca645b
Rory Sterley <rorysterley@gmail.com>
2015-07-29 18:07:13 -0700

    Add static server and dot files

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function() {
+  console.log('\n==================================\n' +
+              '= Server listening on port: ' + port + ' =' +
+              '\n==================================\n');
+});
